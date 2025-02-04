244bae8fd4cab7e41bc203163bf7eb6db7fbad84
Amrendra282 <amrendramishra54@gmail.com>
2025-02-04 10:55:08 +0530

    PracticeGitHub

---
+++ PracticeGitHub/src/test/java/Company/TCSTest.java
@@ -0,0 +1,13 @@
+package Company;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class TCSTest {
+	@Test
+	
+	public void launch() {
+		Reporter.log("TCS Test Launched");
+	}
+
+}
---
+++ PracticeGitHub/src/test/java/Company/WiproTest.java
@@ -0,0 +1,13 @@
+package Company;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class WiproTest {
+	@Test
+	
+	public void launch() {
+		Reporter.log("Wipro Test launched");
+	}
+
+}
