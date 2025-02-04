a3b201aa483d5ea30de3cf2d03df9b5c4bb2f312
kumsu <kumsu@King_Vishwa>
2025-02-04 11:09:15 +0530

    Fork

---
+++ PracticeGitHub/src/test/java/githubfork/ForkAndPull.java
@@ -0,0 +1,13 @@
+package githubfork;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class ForkAndPull 
+{
+	@Test
+public void gitHub() {
+		Reporter.log("TCS Test Launched");
+	
+}
+}
