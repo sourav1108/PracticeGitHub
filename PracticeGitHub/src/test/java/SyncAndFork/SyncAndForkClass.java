2edbddc0f40cb1a640305e86aada5e729d899004
Amrendra282 <amrendramishra54@gmail.com>
2025-02-05 10:23:47 +0530

    SyncAndFork Class added

---
+++ PracticeGitHub/src/test/java/SyncAndFork/SyncAndForkClass.java
@@ -0,0 +1,12 @@
+package SyncAndFork;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class SyncAndForkClass {
+	@Test
+	public void sync() {
+		Reporter.log("SynkAndFork Class added", true);
+	}
+
+}
